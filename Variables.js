50404e151ad90f9b3ac57753119b4a2084b50dcc
Bralex-svg <alexadam269@gmail.com>
2023-02-06 16:31:28 -0800

    added variables demo

---
+++ Variables.js
@@ -0,0 +1,21 @@
+// A variable is a “named storage” for data. We can use variables to store goodies, visitors, and other data.
+
+// To create a variable in JavaScript, use the let keyword.
+
+// The statement below creates (in other words: declares) a variable with the name “message”:
+
+let message;
+// initially, to declare a variable in javascript, you would used the var keyward. But that is no more used because of some reasons which you will get to know later.
+
+//Now we can put some information into it by using the assignment '=' operator
+
+message = "ALEX THE JAVASCRIPT CODER";
+
+//We can declare multiple variables in one line like:
+
+//  let name = 'alex', user ='admin', age = 22;
+
+//But the prefered way is to put them on seperarte lines like:
+let name = "alex";
+let user = "admin";
+let age = 22;
